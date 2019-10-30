226b6bdbad9d0a08137d197defec4b4171ff43db
flcavqueiroz <flcavqueiroz@gmail.com>
2019-10-30 09:46:35 -0300

    atualiza projeto

--- wttd/eventex/wsgi.py
+++ wttd/eventex/wsgi.py
@@ -6,7 +6,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
-
+import sys
 import os
 from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
